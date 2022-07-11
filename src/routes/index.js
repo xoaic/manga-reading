453e2d4919464175b85c9fa118cefcdbb506e22c
xoaic <xoaic@xcc.one>
2022-07-11 11:29:27 +0700

    Update CSS Layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
+import { HeaderOnly } from "../components/Layout";
+
 import Home from '../pages/Home';
 import Profile from "../pages/Profile";
+import PrivacyPolicy from "../pages/PrivacyPolicy";
 
 const publicRoutes = [
-    { path: '/', component: Home }
+    { path: '/', component: Home },
+    { path: '/privacy-policy', component: PrivacyPolicy, layout: HeaderOnly }
 ]
 
 const privateRoutes = [
-    { path: '/profile', component: Profile}
+    { path: '/profile', component: Profile }
 ]
 
 export { publicRoutes, privateRoutes }
